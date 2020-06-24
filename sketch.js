da2810b1a5da235cbc8c4eefeaa538118dce058e
Kheronn K. Machado <kheronn@gmail.com>
2020-06-24 11:56:27 -0300

    Add class animacao.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,47 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
 let cenario;
 let personagem;
 let somDoJogo;
+
+let inimigo;
+const matrizInimigo = [
+  [0, 0],
+  [104, 0],
+  [208, 0],
+  [312, 0],
+  [0, 104],
+  [104, 104],
+  [208, 104],
+  [312, 104],
+  [0, 208],
+  [104, 208],
+  [208, 208],
+  [312, 208],
+  [0, 312],
+  [104, 312],
+  [208, 312],
+  [312, 312],
+  [0, 418],
+  [104, 418],
+  [208, 418],
+  [312, 418],
+  [0, 522],
+  [104, 522],
+  [208, 522],
+  [312, 522],
+  [0, 626],
+  [104, 626],
+  [208, 626],
+  [312, 626],
+]
 
 function preload(){
  
  imagemCenario = loadImage('imagens/floresta.png');
  imagemPersonagem = loadImage('imagens/correndo.png');
+ imagemInimigo = loadImage('imagens/gotinha.png');
  frameRate(40);
 // somDoJogo = loadSound('sons/musica_jogo.ogg');
   
@@ -17,6 +51,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
   personagem = new Personagem(imagemPersonagem);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104);
  // somDoJogo.loop();
 }
 
@@ -24,5 +59,6 @@ function draw() {
   cenario.exibe();
   cenario.move();
   personagem.exibe();
+  inimigo.exibe();
 }
 
